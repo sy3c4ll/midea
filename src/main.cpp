18a0e96a5e5253347f710532dd911e3b80b9f315
sy3c4ll <sy3c4ll@gmail.com>
2021-10-12 19:05:39 +0900

    Normalise inputs, separate loss function, and just procrastinate in general

--- src/main.cpp
+++ src/main.cpp
@@ -22,9 +22,10 @@
 #define EPOCH_SAMPLE_PERIOD 1
 #define SAMPLE_SIZE 10
 #define ACT_FUNC h_swish
+#define NORM_FUNC relu1
 #define NOTE_ON 0x90
 #define T_MIN 0
-#define T_MAX 100000
+#define T_MAX 10
 #define N_MIN 0x00
 #define N_MAX 0x7f
 #define A_MIN 0x01
@@ -46,12 +47,16 @@ inline double min(double x, double y) { return x < y ? x : y; }
 inline double max(double x, double y) { return x > y ? x : y; }
 inline double round(double x) { return (int)(x + 0.5); }
 inline double h_swish(double x) { return x * min(max(x + 3, 0), 6) / 6; }
+inline double relu1(double x) { return min(max(x, 0), 1); }
 
 Eigen::MatrixXd map(Eigen::MatrixXd M, void *func);
 Eigen::VectorXd run(model *m, Eigen::VectorXd x);
-double train(model *m, smf::MidiFile *midi, size_t batch_size);
-smf::MidiFile sample(model *m, Eigen::VectorXd *seq, int seqsize);
+Eigen::VectorXd predict(model *m, Eigen::VectorXd x);
 void play_midi(RtMidiOut *midiout, smf::MidiFile midi);
+double loss(model *m, smf::MidiFile *midi);
+smf::MidiFile sample(model *m, Eigen::VectorXd *seq, int seqsize);
+void train(model *m, smf::MidiFile *midi, size_t batch_size,
+           RtMidiOut *midiout);
 
 int main(int argc, char **argv) {
 
@@ -90,7 +95,7 @@ int main(int argc, char **argv) {
   nlohmann::json md = nlohmann::json::parse(std::ifstream(path));
   std::cout << "[*] Metadata parsed from " << path << std::endl;
 
-  smf::MidiFile midi[MAX_FILE_NUM] = {}, generated;
+  smf::MidiFile midi[MAX_FILE_NUM] = {};
   size_t batch_num = 0;
 
   for (nlohmann::json::reference midi_data : md["midi_filename"]) {
@@ -131,31 +136,11 @@ int main(int argc, char **argv) {
   }
   std::cout << "[*] Connected to MIDI output port " << MIDI_OUT << std::endl;
 
-  double loss = 0;
+  train(m, midi, batch_num, midiout);
 
-  for (size_t epoch = 0; epoch < EPOCH_NUM; epoch++) {
+  std::cout << "[*] Model successfully trained over " << EPOCH_NUM
+            << " epochs, quitting..." << std::endl;
 
-    loss = train(m, midi, batch_num);
-    std::cout << "[*] Epoch " << epoch + 1 << "/" << EPOCH_NUM << " : Loss "
-              << loss << std::endl;
-
-    if (epoch != 0 && epoch % EPOCH_SAMPLE_PERIOD == 0) {
-      std::cout << "[*] Generating MIDI sample for epoch " << epoch << "..."
-                << std::endl;
-      generated = sample(m, {}, 0);
-      std::cout << "[*] MIDI sample successfully generated" << std::endl;
-
-      if (midiout != NULL) {
-        std::cout << "[*] Now playing: MIDI sample for epoch " << epoch
-                  << std::endl;
-        play_midi(midiout, generated);
-      } else {
-        strcpy(path, "sample.mid");
-        generated.write(path);
-        std::cout << "[*] MIDI sample saved to " << path << std::endl;
-      }
-    }
-  }
   delete m;
   delete midiout;
   return 0;
@@ -177,175 +162,17 @@ Eigen::VectorXd run(model *m, Eigen::VectorXd x) {
     m->a[i] =
         map(m->wxy[i] * m->a[i - 1] + m->waa[i] * m->a[i] + m->b[i], ACT_FUNC);
 
-  return map(m->wxy[LAYER_NUM] * m->a[LAYER_NUM - 1] + m->b[LAYER_NUM],
-             ACT_FUNC);
+  return m->wxy[LAYER_NUM] * m->a[LAYER_NUM - 1] + m->b[LAYER_NUM];
 }
 
-double train(model *m, smf::MidiFile *midi, size_t batch_num) {
+Eigen::VectorXd predict(model *m, Eigen::VectorXd x) {
 
-  size_t batch_size = 0;
-  double loss = 0, batch_loss = 0;
-  Eigen::MatrixXd grad_wxy[LAYER_NUM + 1] = {}, grad_waa[LAYER_NUM] = {};
-  Eigen::VectorXd grad_b[LAYER_NUM + 1] = {};
-
-  for (size_t i = 1; i < LAYER_NUM; i++)
-    grad_wxy[i] = Eigen::MatrixXd::Zero(H_SIZE, H_SIZE);
-  grad_wxy[0] = Eigen::MatrixXd::Zero(H_SIZE, IO_SIZE);
-  grad_wxy[LAYER_NUM] = Eigen::MatrixXd::Zero(IO_SIZE, H_SIZE);
-
-  for (size_t i = 0; i < LAYER_NUM; i++)
-    grad_waa[i] = Eigen::MatrixXd::Zero(H_SIZE, H_SIZE);
-
-  for (size_t i = 0; i < LAYER_NUM; i++)
-    grad_b[i] = Eigen::VectorXd::Zero(H_SIZE);
-  grad_b[LAYER_NUM] = Eigen::VectorXd::Zero(IO_SIZE);
-
-  for (size_t batch = 0; batch < batch_num; batch++) {
-
-    batch_size = 0;
-    batch_loss = 0;
-
-    if (batch != 0 && batch % MINI_BATCH_SIZE == 0) {
-      // TODO: finalise loss if needed, calculate gradient functions, and print
-      // a pretty message if need be
-      loss /= MINI_BATCH_SIZE;
-      for (size_t i = 0; i < LAYER_NUM + 1; i++) {
-        m->wxy[i] += grad_wxy[i], m->b[i] += grad_b[i];
-        grad_wxy[i].setZero(), grad_b[i].setZero();
-      }
-      for (size_t i = 0; i < LAYER_NUM; i++) {
-        m->waa[i] += grad_waa[i];
-        grad_waa[i].setZero();
-      }
-      loss = 0;
-    }
-
-    if (!midi[batch].status()) {
-      std::cerr << "[!] train: Unable to parse MIDI data from arguments, "
-                   "skipping: batch "
-                << batch << std::endl;
-      continue;
-    }
-
-    midi[batch].absoluteTicks();
-    midi[batch].joinTracks();
-    midi[batch].doTimeAnalysis();
-    midi[batch].linkNotePairs();
-
-    Eigen::VectorXd x(IO_SIZE), y(IO_SIZE);
-    double ptime = 0;
-    bool init = false;
-
-    for (size_t i = 0; i < (size_t)midi[batch][0].size(); i++)
-      if (midi[batch][0][i].isNoteOn()) {
-
-        batch_size++;
-
-        x << midi[batch][0][i].seconds - ptime,
-            midi[batch][0][i].getDurationInSeconds(), midi[batch][0][i][1],
-            midi[batch][0][i][2];
-
-        if (init)
-          for (size_t j = 0; j < IO_SIZE; j++)
-            batch_loss += (x(j) - y(j)) * (x(j) - y(j));
-
-        y = run(m, x);
-        ptime = midi[batch][0][i].seconds;
-        init = true;
-      }
-
-    batch_loss /= batch_size;
-    loss += batch_loss;
-  }
-
-  loss /= (batch_num % MINI_BATCH_SIZE);
-
-  for (size_t i = 0; i < LAYER_NUM + 1; i++) {
-    m->wxy[i] += grad_wxy[i], m->b[i] += grad_b[i];
-    grad_wxy[i].setZero(), grad_b[i].setZero();
-  }
-  for (size_t i = 0; i < LAYER_NUM; i++) {
-    m->waa[i] += grad_waa[i];
-    grad_waa[i].setZero();
-  }
-
-  return loss;
-}
-
-smf::MidiFile sample(model *m, Eigen::VectorXd *seq, int seqsize) {
-
-  smf::MidiFile midi;
-  std::vector<uint8_t> note{NOTE_ON, 0, 0};
-  Eigen::VectorXd x(IO_SIZE);
-  double atime = 0;
-
-  midi.absoluteTicks();
-  midi.setTPQ(TPQ);
-  midi.addTrack(1);
-
-  if (seqsize) {
-
-    for (size_t i = 0; i < (size_t)seqsize; i++) {
-
-      note[1] = seq[i](2), note[2] = seq[i](3);
-      midi.addEvent(1, (int)((atime + seq[i](0)) * TPQ * BPM / 60), note);
-
-      note[2] = 0;
-      midi.addEvent(1, (int)((atime + seq[i](0) + seq[i](1)) * TPQ * BPM / 60),
-                    note);
-
-      std::cout << "[*] generate: Recorded pre-entered note {" << (int)note[1]
-                << "," << (int)note[2] << "} ON to "
-                << (int)((atime + seq[i](0)) * TPQ * BPM / 60) << " and OFF to "
-                << (int)((atime + seq[i](0) + seq[i](1)) * TPQ * BPM / 60)
-                << std::endl;
-
-      atime += seq[i](0);
-    }
-
-    x = seq[seqsize - 1];
-
-  } else {
-
-    x << 0.5, 0.5, 60, 64;
-
-    note[1] = x(2), note[2] = x(3);
-    midi.addEvent(1, (int)((atime + x(0)) * TPQ * BPM / 60), note);
-
-    note[2] = 0;
-    midi.addEvent(1, (int)((atime + x(0) + x(1)) * TPQ * BPM / 60), note);
-
-    std::cout << "[*] generate: Recorded pre-entered note {" << (int)note[1]
-              << "," << (int)note[2] << "} ON to "
-              << (int)((atime + x(0)) * TPQ * BPM / 60) << " and OFF to "
-              << (int)((atime + x(0) + x(1)) * TPQ * BPM / 60) << std::endl;
-
-    atime += x(0);
-  }
-
-  for (size_t i = 0; i < SAMPLE_SIZE; i++) {
-
-    x = run(m, x);
-    x << min(max(x(0), T_MIN), T_MAX), min(max(x(1), T_MIN), T_MAX),
-        round(min(max(x(2), N_MIN), N_MAX)),
-        round(min(max(x(3), A_MIN), A_MAX));
-
-    note[1] = x(2), note[2] = x(3);
-    midi.addEvent(1, (int)((atime + x(0)) * TPQ * BPM / 60), note);
-
-    note[2] = 0;
-    midi.addEvent(1, (int)((atime + x(0) + x(1)) * TPQ * BPM / 60), note);
-
-    std::cout << "[*] generate: Recorded generated note {" << (int)note[1]
-              << "," << (int)note[2] << "} ON to "
-              << (int)((atime + x(0)) * TPQ * BPM / 60) << " and OFF to "
-              << (int)((atime + x(0) + x(1)) * TPQ * BPM / 60) << std::endl;
-
-    atime += x(0);
-  }
-
-  midi.sortTracks();
-  return midi;
+  x = map(run(m, x), NORM_FUNC);
+  x(0) = round(x(0) * (T_MAX - T_MIN) + T_MIN),
+  x(1) = round(x(1) * (T_MAX - T_MIN) + T_MIN),
+  x(2) = round(x(2) * (N_MAX - N_MIN) + N_MIN),
+  x(3) = round(x(3) * (A_MAX - A_MIN) + A_MIN);
+  return x;
 }
 
 void play_midi(RtMidiOut *midiout, smf::MidiFile midi) {
@@ -369,4 +196,204 @@ void play_midi(RtMidiOut *midiout, smf::MidiFile midi) {
   }
 
   return;
+}
+
+double loss(model *m, smf::MidiFile midi) {
+
+  if (!midi.status()) {
+    std::cerr
+        << "[!] Unable to parse MIDI data from arguments, skipping batch..."
+        << std::endl;
+    return 0;
+  }
+
+  size_t batch_size = 0;
+  double batch_loss = 0;
+
+  midi.absoluteTicks();
+  midi.joinTracks();
+  midi.doTimeAnalysis();
+  midi.linkNotePairs();
+
+  Eigen::VectorXd x(IO_SIZE), y(IO_SIZE);
+  double ptime = 0;
+  bool init = false;
+
+  for (size_t i = 0; i < (size_t)midi[0].size(); i++)
+    if (midi[0][i].isNoteOn()) {
+
+      batch_size++;
+
+      x << ((midi[0][i].seconds - ptime) - T_MIN) / (T_MAX - T_MIN),
+          (midi[0][i].getDurationInSeconds() - T_MIN) / (T_MAX - T_MIN),
+          ((double)midi[0][i][1] - N_MIN) / (N_MAX - N_MIN),
+          ((double)midi[0][i][2] - A_MIN) / (A_MAX - A_MIN);
+
+      if (init)
+        for (size_t j = 0; j < IO_SIZE; j++)
+          batch_loss += (x(j) - y(j)) * (x(j) - y(j));
+
+      y = run(m, x);
+      ptime = midi[0][i].seconds;
+      init = true;
+    }
+
+  return batch_loss / batch_size;
+}
+
+smf::MidiFile sample(model *m, Eigen::VectorXd *seq, int seqsize) {
+
+  smf::MidiFile midi;
+  std::vector<uint8_t> note{NOTE_ON, 0, 0};
+  Eigen::VectorXd x(IO_SIZE), n(IO_SIZE);
+  double atime = 0;
+
+  midi.absoluteTicks();
+  midi.setTPQ(TPQ);
+  midi.addTrack(1);
+
+  if (seqsize) {
+
+    for (size_t i = 0; i < (size_t)seqsize; i++) {
+
+      note[1] = seq[i](2), note[2] = seq[i](3);
+      midi.addEvent(1, (int)((atime + seq[i](0)) * TPQ * BPM / 60), note);
+
+      note[2] = 0;
+      midi.addEvent(1, (int)((atime + seq[i](0) + seq[i](1)) * TPQ * BPM / 60),
+                    note);
+
+      std::cout << "[*] generate: Recorded pre-entered note " << (int)note[1]
+                << " of attack " << (int)note[2] << " ON to "
+                << (int)((atime + seq[i](0)) * TPQ * BPM / 60) << " and OFF to "
+                << (int)((atime + seq[i](0) + seq[i](1)) * TPQ * BPM / 60)
+                << std::endl;
+
+      atime += seq[i](0);
+    }
+
+    x = seq[seqsize - 1];
+
+  } else {
+
+    x << 0.5, 0.5, 60, 64;
+
+    note[1] = x(2), note[2] = x(3);
+    midi.addEvent(1, (int)((atime + x(0)) * TPQ * BPM / 60), note);
+
+    note[2] = 0;
+    midi.addEvent(1, (int)((atime + x(0) + x(1)) * TPQ * BPM / 60), note);
+
+    std::cout << "[*] generate: Recorded pre-entered note " << (int)note[1]
+              << " of attack " << (int)note[2] << " ON to "
+              << (int)((atime + x(0)) * TPQ * BPM / 60) << " and OFF to "
+              << (int)((atime + x(0) + x(1)) * TPQ * BPM / 60) << std::endl;
+
+    atime += x(0);
+  }
+
+  for (size_t i = 0; i < SAMPLE_SIZE; i++) {
+
+    n = predict(m, x);
+    x = run(m, x);
+
+    note[1] = n(2), note[2] = n(3);
+    midi.addEvent(1, (int)((atime + n(0)) * TPQ * BPM / 60), note);
+
+    note[2] = 0;
+    midi.addEvent(1, (int)((atime + n(0) + n(1)) * TPQ * BPM / 60), note);
+
+    std::cout << "[*] generate: Recorded generated note " << (int)note[1]
+              << " of attack " << (int)note[2] << " ON to "
+              << (int)((atime + n(0)) * TPQ * BPM / 60) << " and OFF to "
+              << (int)((atime + n(0) + n(1)) * TPQ * BPM / 60) << std::endl;
+
+    atime += n(0);
+  }
+
+  midi.sortTracks();
+  return midi;
+}
+
+void train(model *m, smf::MidiFile *midi, size_t batch_num,
+           RtMidiOut *midiout) {
+
+  double cost = 0;
+  model grad;
+  smf::MidiFile sample_midi;
+  char path[MAX_PATH_LEN] = "";
+
+  for (size_t i = 1; i < LAYER_NUM; i++)
+    grad.wxy[i] = Eigen::MatrixXd::Zero(H_SIZE, H_SIZE);
+  grad.wxy[0] = Eigen::MatrixXd::Zero(H_SIZE, IO_SIZE);
+  grad.wxy[LAYER_NUM] = Eigen::MatrixXd::Zero(IO_SIZE, H_SIZE);
+
+  for (size_t i = 0; i < LAYER_NUM; i++)
+    grad.waa[i] = Eigen::MatrixXd::Zero(H_SIZE, H_SIZE);
+
+  for (size_t i = 0; i < LAYER_NUM; i++)
+    grad.b[i] = Eigen::VectorXd::Zero(H_SIZE);
+  grad.b[LAYER_NUM] = Eigen::VectorXd::Zero(IO_SIZE);
+
+  for (size_t epoch = 0; epoch < EPOCH_NUM; epoch++) {
+
+    cost = 0;
+
+    for (size_t batch = 0; batch < batch_num; batch++) {
+
+      if (batch != 0 && batch % MINI_BATCH_SIZE == 0) {
+        // TODO: calculate gradient functions and report to stdout
+        cost /= MINI_BATCH_SIZE;
+        for (size_t i = 0; i < LAYER_NUM + 1; i++) {
+          m->wxy[i] += grad.wxy[i], m->b[i] += grad.b[i];
+          grad.wxy[i].setZero(), grad.b[i].setZero();
+        }
+        for (size_t i = 0; i < LAYER_NUM; i++) {
+          m->waa[i] += grad.waa[i];
+          grad.waa[i].setZero();
+        }
+        cost = 0;
+      }
+
+      if (!midi[batch].status()) {
+        std::cerr << "[!] train: Unable to parse MIDI data from arguments, "
+                     "skipping: batch "
+                  << batch << std::endl;
+        continue;
+      }
+      cost += loss(m, midi[batch]);
+    }
+
+    cost /= ((batch_num % MINI_BATCH_SIZE) ? batch_num % MINI_BATCH_SIZE
+                                           : MINI_BATCH_SIZE);
+
+    for (size_t i = 0; i < LAYER_NUM + 1; i++) {
+      m->wxy[i] += grad.wxy[i], m->b[i] += grad.b[i];
+      grad.wxy[i].setZero(), grad.b[i].setZero();
+    }
+    for (size_t i = 0; i < LAYER_NUM; i++) {
+      m->waa[i] += grad.waa[i];
+      grad.waa[i].setZero();
+    }
+
+    std::cout << "[*] Epoch " << epoch + 1 << "/" << EPOCH_NUM << " : Loss "
+              << cost << std::endl;
+
+    if (epoch != 0 && epoch % EPOCH_SAMPLE_PERIOD == 0) {
+      std::cout << "[*] Generating MIDI sample for epoch " << epoch + 1 << "..."
+                << std::endl;
+      sample_midi = sample(m, {}, 0);
+      std::cout << "[*] MIDI sample successfully generated" << std::endl;
+
+      if (midiout != NULL) {
+        std::cout << "[*] Now playing: MIDI sample for epoch " << epoch + 1
+                  << std::endl;
+        play_midi(midiout, sample_midi);
+      } else {
+        strcpy(path, "sample.mid");
+        sample_midi.write(path);
+        std::cout << "[*] MIDI sample saved to " << path << std::endl;
+      }
+    }
+  }
 }
